8288a21f0b01fc4ec8661a496ffe1f0cc9b43c87
Cristian <cristianfierrosilva@gmail.com>
2024-05-01 18:15:27 -0500

    add header component

---
+++ src/modules/home/module.vue
@@ -0,0 +1,10 @@
+<template>
+  <Header />
+  <div class="min-h-[calc(100vh-82px)]  bg-neutral-gray ">
+    <router-view />
+  </div>
+</template>
+
+<script setup>
+import Header from "./common/Header.vue";
+</script>
